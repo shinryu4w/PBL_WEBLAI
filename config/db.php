2c18e91c379253230362b88a4d145e0e13fe65f1
SyahrulBhudiF <syahrul4w@gmail.com>
2025-11-29 11:02:53 +0700

    prod: prod prod

--- config/db.php
+++ config/db.php
@@ -1,13 +1,15 @@
 <?php
 require_once dirname(__DIR__) . "/vendor/autoload.php";
 
-// Load .env
-// $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
-// $dotenv->load();
-
 try {
-    $dsn = "pgsql:host={$_ENV["DB_HOST"]};port={$_ENV["DB_PORT"]};dbname={$_ENV["DB_NAME"]}";
-    $pdo = new PDO($dsn, $_ENV["DB_USER"], $_ENV["DB_PASS"], [
+    $dsn =
+        "pgsql:host=" .
+        getenv("DB_HOST") .
+        ";port=" .
+        getenv("DB_PORT") .
+        ";dbname=" .
+        getenv("DB_NAME");
+    $pdo = new PDO($dsn, getenv("DB_USER"), getenv("DB_PASS"), [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
         PDO::ATTR_EMULATE_PREPARES => false,
