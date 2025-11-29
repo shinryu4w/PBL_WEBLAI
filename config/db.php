a711bdedaaad1f244684e1ada542232998f3b897
SyahrulBhudiF <syahrul4w@gmail.com>
2025-11-29 10:53:54 +0700

    prod: prod prod

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-require_once __DIR__ . "/vendor/autoload.php";
+require_once dirname(__DIR__) . "/vendor/autoload.php";
 
 // Load .env
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
 $dotenv->load();
 
 try {
